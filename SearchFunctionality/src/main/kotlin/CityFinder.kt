52f795767aac4ba388bcbb8ee343f597bdaa1ee3
Koimi YOVO <yovoedem33@gmail.com>
2021-10-10 17:28:36 +0200

    search functionality: implémentation complète de la fonctionnalité

--- SearchFunctionality/src/main/kotlin/CityFinder.kt
+++ SearchFunctionality/src/main/kotlin/CityFinder.kt
@@ -1,11 +1,19 @@
 class CityFinder(private val cities: List<String>)
 {
+    companion object
+    {
+        private const val ALL_CITIES_WILDCARD = "*"
+    }
+
     fun find(cityName: String): List<String>
     {
         return when
         {
-            cities.isEmpty() -> emptyList()
-            else             -> cities.filter { it == cityName }
+            cityName == ALL_CITIES_WILDCARD -> cities
+            searchCriteriaIsValid(cityName) -> cities.filter { it.contains(cityName, ignoreCase = true) }
+            else                            -> emptyList()
         }
     }
+
+    private fun searchCriteriaIsValid(cityName: String) = cities.isNotEmpty() && cityName.length > 1
 }
--- SearchFunctionality/src/test/kotlin/CityFinderTest.kt
+++ SearchFunctionality/src/test/kotlin/CityFinderTest.kt
@@ -4,20 +4,64 @@ import kotlin.test.assertEquals
 
 class CityFinderTest
 {
+    private val cities = listOf("Lyon", "Paris", "Lomé", "Hong Kong", "Accra", "Bangkok")
+    private val cityFinder = CityFinder(cities)
+
     @Test
     internal fun `empty referential`()
     {
-        val cityFinder = CityFinder(emptyList())
-        assertEquals(emptyList(), cityFinder.find("Paris"))
+        val emptyCityFinder = CityFinder(emptyList())
+        assertEquals(emptyList(), emptyCityFinder.find("Paris"))
     }
 
     @Test
     internal fun `given city name in database then get it`()
     {
-        val cityFinder = CityFinder(listOf("Lyon", "Paris", "Lomé"))
         assertAll(
                 { assertEquals(listOf("Paris"), cityFinder.find("Paris")) },
                 { assertEquals(listOf("Lomé"), cityFinder.find("Lomé")) }
         )
+    }
+
+    @Test
+    internal fun `should get all city names starting with search text`()
+    {
+        assertAll(
+                { assertEquals(listOf("Lyon"), cityFinder.find("Ly")) },
+                { assertEquals(listOf("Paris"), cityFinder.find("Pa")) }
+        )
+    }
+
+    @Test
+    internal fun `given search text less than 2 characters then get empty list`()
+    {
+        assertAll(
+                { assertEquals(emptyList(), cityFinder.find("")) },
+                { assertEquals(emptyList(), cityFinder.find("P")) }
+        )
+    }
+
+    @Test
+    internal fun `search text is case insensitive`()
+    {
+        assertAll(
+                { assertEquals(listOf("Accra"), cityFinder.find("accra")) },
+                { assertEquals(listOf("Lyon"), cityFinder.find("lY")) }
+        )
+    }
+
+    @Test
+    internal fun `should get all city names containing search text`()
+    {
+        assertAll(
+                { assertEquals(listOf("Lomé"), cityFinder.find("om")) },
+                { assertEquals(listOf("Hong Kong", "Bangkok"), cityFinder.find("ng")) },
+        )
+    }
+
+    @Test
+    internal fun `given asterisk as search text then get all city names`()
+    {
+        assertEquals(cities, cityFinder.find("*"))
     }
 }
